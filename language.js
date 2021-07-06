afec362eca9a0ed967611dac620dc0e8220604e4
extrem9713 <extrem9713@gmail.com>
2021-07-06 19:21:52 +0800

    update json

--- language.js
+++ language.js
@@ -1,21 +1,5 @@
-const target = {
-  engineer:{
-    title:'工程師'
-  },
-  designer:{
-    title:'設計師'
-  },
-  entrepreneur:{
-    title:'創業家'
-  }
-}
-const task = {
-  engineer: ['加個按鈕','加新功能','切個版', '改一點 code'],
-  designer: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
-  entrepreneur: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
-
-}
-const phrase = ['很簡單','很容易','很快','很正常']
+const person =require('./people.json')
+const phrase = ['很簡單','很容易','很快','很正常','沒問題的','辦不到嗎','很困難嗎']
 
 function sample (array) {
   const index = Math.floor(Math.random() * array.length)
@@ -23,16 +7,12 @@ function sample (array) {
 }
 
 function generateLanguage (options) {
-  if (!options.job){return '請選一個人物!'}
-  if (options.job === 'engineer') {
-    return `身為一個${target[options.job].title},${sample(task[options.job])},${sample(phrase)}。`
+  if (!options.job){return '請選一個人物!'}else{
+   return `身為一個${person.target[options.job].title},只是${sample(person.target[options.job].task)},${sample(person.phrase)}`
   }
-   if (options.job=== 'designer') {
-    return `身為一個${target[options.job].title},${sample(task[options.job])},${sample(phrase)}。`
-  }
-   if (options.job === 'entrepreneur') {
-    return `身為一個${target[options.job].title},${sample(task[options.job])},${sample(phrase)}。`
-  }
+    
 }
+
+
 module.exports = generateLanguage
 
